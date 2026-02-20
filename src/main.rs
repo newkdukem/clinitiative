15af4f8856bc82ffd72db28f0d10e1b6ab2a7c49
Nikola Demerdjiev <newkdukem@gmail.com>
2026-02-20 18:21:30 +0100

    add who's turn is it and rudimentary controls for next/previous combatant turn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use ratatui::{DefaultTerminal, Frame};
+use crossterm::event::{self, Event, KeyCode};
 use ratatui::text::Text;
+use ratatui::{DefaultTerminal, Frame};
 
 struct Combatant {
     name: String,
@@ -8,25 +9,31 @@ struct Combatant {
 
 struct App {
     combatants: Vec<Combatant>,
+    current_turn: usize,
 }
 
 impl App {
     fn new() -> Self {
+        let mut combatants = vec![
+            Combatant {
+                name: String::from("Frodo"),
+                initiative: 8,
+            },
+            Combatant {
+                name: String::from("Sam"),
+                initiative: 16,
+            },
+            Combatant {
+                name: String::from("Gollum"),
+                initiative: 11,
+            },
+        ];
+
+        combatants.sort_by(|a, b| b.initiative.cmp(&a.initiative));
+
         App {
-            combatants: vec![
-                Combatant {
-                    name: String::from("Frodo"),
-                    initiative: 8,
-                },
-                Combatant {
-                    name: String::from("Sam"),
-                    initiative: 16,
-                },
-                Combatant {
-                    name: String::from("Gollum"),
-                    initiative: 11,
-                },
-            ],
+            combatants: combatants,
+            current_turn: 0,
         }
     }
 }
@@ -38,15 +45,26 @@ fn main() -> color_eyre::Result<()> {
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
-    let app = App::new();
+    let mut app = App::new();
     loop {
         terminal.draw(|frame| {
             // capture app by reference
             let app = &app;
             render(app, frame);
         })?;
-        if crossterm::event::read()?.is_key_press() {
-            break Ok(());
+        match event::read()? {
+            Event::Key(key_event) => match key_event.code {
+                KeyCode::Down => {
+                    app.current_turn = (app.current_turn + 1) % app.combatants.len();
+                }
+                KeyCode::Up => {
+                    app.current_turn =
+                        (app.current_turn + app.combatants.len() - 1) % app.combatants.len();
+                }
+                KeyCode::Char('q') => break Ok(()),
+                _ => {}
+            },
+            _ => {}
         }
     }
 }
@@ -55,7 +73,11 @@ fn render(app: &App, frame: &mut Frame) {
     let names: String = app
         .combatants
         .iter()
-        .map(|c| format!("{}: {}",c.name.as_str(), c.initiative))
+        .enumerate()
+        .map(|(i, c)| {
+            let prefix = if i == app.current_turn { ">> " } else { "" };
+            format!("{}{}: {}", prefix, c.name.as_str(), c.initiative)
+        })
         .collect::<Vec<String>>()
         .join("\n");
     let text = Text::raw(names);
